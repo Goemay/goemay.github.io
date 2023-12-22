e4cc638df751e6ef311e296d902924157e2eb8cd
Jimraihan <raihangumay02@gmail.com>
2023-12-22 14:05:00 +0700

    mengubah background

--- script.js
+++ script.js
@@ -174,3 +174,13 @@ window.addEventListener('scroll', function() {
   }
 }
 });
+
+window.addEventListener('scroll', function() {
+  var header = document.getElementById('hamburger-nav');
+
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
